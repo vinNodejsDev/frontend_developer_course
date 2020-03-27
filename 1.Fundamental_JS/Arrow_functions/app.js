d232474285de05eaa491fa2e9cde0c739c67d22a
John Wo <labmansoftac@gmail.com>
2020-03-27 23:05:37 +0200

    Arrow functions lesson

---
+++ 1.Fundamental_JS/Arrow_functions/app.js
@@ -0,0 +1,43 @@
+const plus = (a, b) => a + b;
+// console.log(plus(23, 42));
+
+// function plus(a, b) {
+//     return a + b;
+// }
+
+const withoutArgs = () => console.log('Hello World!');
+
+const single = (x=1) => x * x;
+
+//multiline
+const moreActions = (a, b) => {
+    a *= 3;
+    b *= 4;
+    return a + b;
+};
+
+const returnObj = (str='') => ({
+    value: str,
+    length: str.length
+});
+// console.table(returnObj('Vitalik'));
+
+
+// function plusFoo(a, b) {
+//     console.log(arguments);
+//     return a + b;
+// }
+//
+// plusFoo(1,3,42,21,13,12);
+
+const obj = {
+    age: 32,
+    firstName: 'Vitalii',
+    getFirstName() {
+        console.log(this)
+    },
+    getAge: () => console.log(this)
+};
+
+// obj.getFirstName();
+obj.getAge();
