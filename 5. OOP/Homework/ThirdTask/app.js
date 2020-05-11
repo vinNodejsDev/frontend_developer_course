7102cec9b07e0661d0754fddfbcc2fc82673de6b
John Wo <labmansoftac@gmail.com>
2020-05-12 00:22:27 +0300

    OOP homeworks

--- 5. OOP/Homework/ThirdTask/app.js
+++ 5. OOP/Homework/ThirdTask/app.js
@@ -10,3 +10,34 @@ Furniture.prototype.getInfo = function() {
     return 'Name: ' + this.name + '. \nPrice: ' + this.price;
 };
 
+const chair = new Furniture('chair', 240);
+
+function OfficeFurniture(name, price, placeForComputer=false) {
+    Furniture.apply(this, arguments);
+    this.placeForComputer = placeForComputer;
+}
+
+OfficeFurniture.prototype = Object.create(Furniture.prototype);
+OfficeFurniture.prototype.constructor = OfficeFurniture;
+
+OfficeFurniture.prototype.getInfo = function(){
+    const resString = this.placeForComputer ? 'There is a place for computer' : 'There is no place for computer';
+    return {name: this.name, Price: this.price, additions: resString};
+};
+
+const table = new OfficeFurniture('table', 840, true);
+
+function HomeFurniture(name, price, isCozzy=true) {
+    Furniture.apply(this, arguments);
+    this.isCozzy = isCozzy;
+}
+
+HomeFurniture.prototype = Object.create(Furniture.prototype);
+HomeFurniture.constructor = HomeFurniture;
+
+HomeFurniture.prototype.getInfo = function() {
+    const resString = this.placeForComputer ? 'Cozzy furniture' : 'Usual furniture';
+    return `${this.name}, ${this.price}, ${resString}`
+}
+
+const bed = new HomeFurniture('bed', 3290, true);
